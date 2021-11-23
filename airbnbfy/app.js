28c0f78c3aafa450a5ca567d875f9873e52c5cbc
Songhareem <ignisilva92@gmail.com>
2021-11-23 22:16:32 +0900

    Feat: Add basic view And event handler test
    Modified index.html / Add basic view
    Modified app.js / test event handler

--- airbnbfy/app.js
+++ airbnbfy/app.js
@@ -1,1 +1,7 @@
-console.log("test air");
+const beforeText = document.getElementById("before");
+const afterText = document.getElementById("after");
+const convertButton = document.getElementById("convertButton");
+
+convertButton.addEventListener("click", function () {
+  console.log("test");
+});
