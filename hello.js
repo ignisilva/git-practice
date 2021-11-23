84f4ef24acb989c5734ab27706ebbd836f5b9f8f
Songhareem <ignisilva92@gmail.com>
2021-11-23 21:36:52 +0900

    Fix: hello.js
    conflict error fix

--- hello.js
+++ hello.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-
 for(let i=0; i<10; i++)
 	if(i%2 === 0)
 		console.log("Conflict1 and main branch"+i);
 	else 
 		console.log(i);
-=======
-console.log("Init Branch");
->>>>>>> parent of 75e8036 (Conflict test: Add conditional statement(conflict1))
