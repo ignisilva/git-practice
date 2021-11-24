5f31cb0e22ae1acb5886509e2292c36ddc2ffc09
Songhareem <ignisilva92@gmail.com>
2021-11-24 12:01:47 +0900

    Feat: convert sentence
    Modified app.js
    
    for convert sentence (normal -> strange)

--- airbnbfy/app.js
+++ airbnbfy/app.js
@@ -3,12 +3,28 @@ const afterText = document.getElementById("after");
 const convertButton = document.getElementById("convertButton");
 
 beforeText.addEventListener("click", function () {
-  beforeText.value = "";
+  init();
 });
 
 convertButton.addEventListener("click", function () {
-  const before = beforeText.value;
-  console.log(before);
+  const beforeValue = beforeText.value;
+  const converted = beforeValue.split("").map((value) => {
+    let convertedValue = value;
+    if ("\uAC00" <= value && value <= "\uD7AF") {
+      const randomValue = (Math.ceil(Math.random() * 10) % 2) + 1;
+
+      let unicode = String.fromCharCode(value.charCodeAt(0) + randomValue);
+
+      if (unicode < "\uAC00" || "\uD7AF" < unicode) {
+        unicode = String.fromCharCode(value.charCodeAt(0) + -1 * randomValue);
+      }
+
+      convertedValue = unicode;
+    }
+    return convertedValue;
+  });
+
+  afterText.value = converted.join("");
 });
 
 function init() {
